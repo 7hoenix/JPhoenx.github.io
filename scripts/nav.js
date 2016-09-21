c8abf61cbf48c2e6007f8b20b7b1a1f06b1285ea
jphoenix86 <jpholzmann@gmail.com>
2016-09-21 14:01:27 -0700

    Compiled Site.

---
+++ scripts/nav.js
@@ -0,0 +1,36 @@
+(function() {
+  var addClassToElement, removeClassFromElement, toggleElements;
+
+  window.addEventListener('scroll', function(ev) {
+    var distanceToTop, introContainerDistance, navContainerDistance, smallSpaceDistance;
+    introContainerDistance = document.getElementsByClassName("intro-header")[0].clientHeight;
+    smallSpaceDistance = document.getElementsByClassName("space-extra-small")[0].clientHeight;
+    navContainerDistance = smallSpaceDistance + introContainerDistance;
+    distanceToTop = window.pageYOffset;
+    console.log("nav: " + navContainerDistance);
+    console.log("distance to top: " + distanceToTop);
+    return toggleElements(distanceToTop, navContainerDistance);
+  });
+
+  toggleElements = function(distanceToTop, navContainerDistance) {
+    var entry, nav;
+    nav = document.getElementsByClassName("section-nav")[0];
+    entry = document.getElementById("currentEntry");
+    if (distanceToTop >= navContainerDistance) {
+      addClassToElement(nav, 'fixed-nav');
+      return addClassToElement(entry, 'nav-padding');
+    } else {
+      removeClassFromElement(nav, 'fixed-nav');
+      return removeClassFromElement(entry, 'nav-padding');
+    }
+  };
+
+  addClassToElement = function(element, classToAdd) {
+    return element.classList.add(classToAdd);
+  };
+
+  removeClassFromElement = function(element, classToRemove) {
+    return element.classList.remove(classToRemove);
+  };
+
+}).call(this);
